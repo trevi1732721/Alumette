53327f3c98dc2d8b6c6b5ca284b03f20525ca0d1
TreVi1732721 <vincent2000.tremblay@videotron.ca>
2018-01-18 14:01:45 -0500

    version 1 ordre des joueurs ne fonctionne pas, mais le reste semble bon

---
+++ src/main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+/**
+ * Created by TreVi1732721 on 2018-01-15.
+ */
+public class main {
+    public static void main(String[] args) {
+        //jeu des Allumette
+        int nbAllumette = (int) (Math.random() * 80) + 20;
+        String joueur0 = " ";
+        String joueur1 = " ";
+        byte premier = (byte) (Math.random() * 2);
+        String[] joueurs = new String[2];
+        int nbTours = 0;
+        byte retrait;
+        boolean continuer = true;
+        boolean choix = true;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Joueur 1, quel est votre nom?");
+        joueurs[0] = sc.nextLine();
+        System.out.println(" Joueur 2, quel est votre nom?");
+        joueurs[1] = sc.nextLine();
+        while (continuer) {
+            while (nbAllumette > 0) {
+                if (nbTours == 0) {
+                    nbTours = nbTours + premier;
+                }
+
+                if (nbTours % 2 == 0) {
+                    while (choix) {
+                        System.out.println(joueurs[0] + ", combien d'allumette voulez vous retirer?(un, deux ou trois)");
+                        retrait = sc.nextByte();
+                        if (retrait == 1 || retrait == 2 || retrait == 3) {
+                            choix = false;
+                            nbAllumette = nbAllumette-retrait;
+                            joueur0 += retrait + ", ";
+                        }
+                    }
+                }
+                if (nbTours % 2 == 1) {
+                    choix = true;
+                    while (choix) {
+                        System.out.println(joueurs[1] + ", combien d'allumette voulez vous retirer?(un, deux ou trois)");
+                        retrait = sc.nextByte();
+                        if (retrait == 1 || retrait == 2 || retrait == 3) {
+                            choix = false;
+                            nbAllumette = nbAllumette-retrait;
+                            joueur1 += retrait + ", ";
+                        }
+                    }
+                }
+                nbTours = nbTours+1;
+                }
+            System.out.println(joueurs[nbTours % 2] + " gagne la partie!, \n Résumé de la partie (coups jouées dans l'ordre...)");
+            System.out.println(joueurs[0] + " :" + joueur0);
+            System.out.println(joueurs[1] + " :" + joueur1);
+            choix = true;
+            while (choix) {
+                System.out.println("voulez vous continuer à jouer? \n 1- Oui\n 2- Non");
+                nbTours = sc.nextInt();
+                if (nbTours == 2) {
+                    continuer = false;
+                    choix = false;
+                } else if (nbTours == 1) {
+                    choix = false;
+                }
+            }
+
+        }
+    }
+}
